c499ed1b71c36e54286d324c24f71a1d3beef6a1
Marcuses <marcusthetechguru@gmail.com>
2022-10-31 03:47:03 -0400

    Added Oauth2 Github functionality

--- chat/main.go
+++ chat/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"github.com/marcusbello/oauth-chat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"html/template"
 	"log"
 	"net/http"
@@ -23,7 +28,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("template", t.filename)))
 	})
-	err := t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	err := t.templ.Execute(w, data)
 	if err != nil {
 		return
 	}
@@ -32,11 +43,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse()
+	//setup gomniauth
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret",
+			"http://localhost:8080/auth/callback/facebook"),
+		github.New("7a5b8031ea5b5fdafc1a",
+			"451e709004f829e81856bbf820a7b84e6634ef52",
+			"http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret",
+			"http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	//endpoints
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	//get the room going
 	go r.run()
---
+++ chat/message.go
@@ -0,0 +1,4 @@
+package main
+
+type message struct {
+}
